53effe8c31dfa26f23386289b9aac39300965e50
bowen <bowen.huang@yale.edu>
2019-11-13 23:40:23 -0500

    update perl

--- perl/play.pl
+++ perl/play.pl
@@ -6,6 +6,17 @@ use warnings;
 use lib $ENV{'VERONICA_PERL'};
 use Veronica::Common;
 use Veronica::Threads;
+use File::Copy qw(move);
 
 Veronica::Common::set_msg_level(5);
-Veronica::Common::say_level('4', 'lalala');
+
+chdir("/Users/bowen/Library/Mobile\ Documents/JFJWWP64QD~com~goodiware~GoodReader/Documents/@\ Papers");
+
+my @pdfs = `find . -type f`;
+
+foreach my $original_path (@pdfs)
+{
+    if(($original_path =~ /\.pdf$/))
+    {
+    }
+}
