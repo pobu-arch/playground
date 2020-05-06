136184d0dd66525e3b9185f36f63125bdda62455
bowen <bowen.huang@yale.edu>
2020-05-06 12:13:57 -0400

    update kernel

--- cpp/cuda/matmul.cu
+++ cpp/cuda/matmul.cu
@@ -1,29 +1,21 @@
-
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
 #include <time.h>
 #include <cuda.h>
-
-#ifdef GPU_PART
-    //#define GPU_Task1
-    #define GPU_Task2
-    //#define GPU_Task34
-    
-#endif
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
 
 #define NO_CMD_INPUT
 #ifdef NO_CMD_INPUT
-    #define M        8192
-    #define N        2048
-    #define P        2048    
+    #define M        1024
+    #define N        1024
+    #define P        1024
     #define BlockDim 32
-    #define GridDim  (N / BlockDim)
+    #define GridDim  (N / BlockDim + 1)
     #define TILE_WIDTH 16
+    #define ADJ_FACTOR 3
 #endif
 
 //#define CPU_PART
@@ -31,7 +23,13 @@
 #define CHECK_PART
 //#define PRINT_PART
 
-#define rand_max 32768
+#ifdef GPU_PART
+#define GPU_Task1
+//#define GPU_Task2
+//#define GPU_Task34
+#endif
+
+#define rand_max 7
 #define FP float
 
 void print_matrix(FP* a, int m, int n)
@@ -108,6 +106,56 @@ __global__ void gpu_kernel_task2(FP* a, FP* b, FP* c, int m, int n, int p)
 
     if (row < m && col < p)
         c[row * p + col] = v;
+}
+
+__global__ void gpu_kernel_task34(FP* a, FP* b, FP* c, int m, int n, int p)
+{
+    __shared__ float sharedA[TILE_WIDTH][TILE_WIDTH];
+    __shared__ float sharedB[TILE_WIDTH][ADJ_FACTOR * TILE_WIDTH];
+    
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+    int row = by * TILE_WIDTH + ty;
+    int col = bx * TILE_WIDTH + tx;
+    float v1 = 0.0, v2 = 0.0;
+
+    for (int i = 0; i < (int)(ceil((float)n / TILE_WIDTH)); i++)
+    {
+        if (i * TILE_WIDTH + tx < n && row < m)
+            sharedA[ty][tx] = a[row * n + i * TILE_WIDTH + tx];
+        else
+            sharedA[ty][tx] = 0.0;
+
+        if (i * TILE_WIDTH + ty < n && col < p)
+        {
+            sharedB[ty][tx] = b[(i * TILE_WIDTH + ty) * p + col];
+            if (col + p / 2 < p)
+            {
+                sharedB[ty][tx + TILE_WIDTH] = b[(i * TILE_WIDTH + ty) * p + col + p / 2];
+            }
+        }
+        else
+            sharedB[ty][tx] = 0.0;
+        __syncthreads();
+
+        for (int j = 0; j < TILE_WIDTH; j++)
+        {
+            v1 += sharedA[ty][j] * sharedB[j][tx];
+            v2 += sharedA[ty][j] * sharedB[j][tx + TILE_WIDTH];
+        }
+        __syncthreads();
+    }
+
+    if (row < m && col < p)
+    {
+        c[row * p + col] = v1;
+        if (col + p / 2 < p)
+        {
+            c[row * p + col + p / 2] = v2;
+        }
+    }
 }
 
 void kij_matrixmult(FP* a, FP* b, FP* kij, int m, int n, int p)
@@ -260,19 +308,30 @@ int main(int argc, char* argv[])
     cudaMemcpy(dev_a, a, m * n * sizeof(FP), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, n * p * sizeof(FP), cudaMemcpyHostToDevice);
 
+    printf("Matrix Dimension (m, n, p) = (%d, %d, %d)\n", m, n, p);
+
     cudaEventRecord(start, 0);
     // cudaEventSynchronize(start); // not needed
 
 #ifdef GPU_Task1
+    printf("Using gpu_kernel_task1 \n");
     dim3 DimGrid((p - 1) / BlockDim + 1, (m - 1) / BlockDim + 1, 1);
     dim3 DimBlock(BlockDim, BlockDim, 1);
     gpu_kernel_task1 <<<DimGrid, DimBlock >>> (dev_a, dev_b, dev_c, m, n, p);
 #endif    
 
 #ifdef GPU_Task2
+    printf("Using gpu_kernel_task2 \n");
     dim3 DimGrid(ceil(p / BlockDim), ceil(m / BlockDim), 1);
     dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1);
-    gpu_kernel_task2 << < dimGrid, dimBlock >> > (dev_a, dev_b, dev_c, m, n, p, TILE_WIDTH);
+    gpu_kernel_task2 <<< DimGrid, DimBlock >>> (dev_a, dev_b, dev_c, m, n, p);
+#endif
+
+#ifdef GPU_Task34
+    printf("Using gpu_kernel_task34 \n");
+    dim3 DimGrid(ceil(p / BlockDim / ADJ_FACTOR ), ceil(m / BlockDim), 1);
+    dim3 DimBlock(TILE_WIDTH, TILE_WIDTH, 1);
+    gpu_kernel_task34 <<< DimGrid, DimBlock >>> (dev_a, dev_b, dev_c, m, n, p);
 #endif
 
     cudaEventRecord(stop, 0); // instrument code to measure end time
@@ -281,9 +340,7 @@ int main(int argc, char* argv[])
 
     cudaMemcpy(gpu, dev_c, m * p * sizeof(FP), cudaMemcpyDeviceToHost);
 
-    printf("Matrix Dimension = %d\n", n);
     printf("Block_Dim = %d, Grid_Dim = %d\n", Block_Dim, Grid_Dim);
-
     printf("Time to calculate results on GPU: %f ms.\n", elapsed_time_ms); // exec. time
 #endif
 
@@ -301,17 +358,40 @@ int main(int argc, char* argv[])
     cudaEventElapsedTime(&elapsed_time_ms, start, stop);
 
     printf("Time to calculate kij results on CPU: %f ms.\n", elapsed_time_ms); // exec. time
+
+    
 #endif
 
 #ifdef CHECK_PART
-    check_matrix(a, b, gpu, m, n, p);
+    
+    #ifdef PRINT_PART
+        printf("\n");
+        printf("Matrix A:\n");
+        print_matrix(a, m, n);
+        printf("Matrix B:\n");
+        print_matrix(b, n, p);
+        
+        #ifdef GPU_PART
+        printf("GPU Result:\n");
+        print_matrix(gpu, m, p);
+        #endif
+        
+        #ifdef CPU_PART
+        printf("CPU Result:\n");
+        print_matrix(kij, m, p);
+        #endif
+    #endif
+
+    #ifdef CPU_PART
+        check_matrix(a, b, kij, m, n, p);
+    #endif    
+
+    #ifdef GPU_PART
+        check_matrix(a, b, gpu, m, n, p);
+    #endif 
 #endif
 
-#ifdef PRINT_PART
-    print_matrix(a, m, n);
-    print_matrix(b, n, p);
-    print_matrix(kij, m, p);
-#endif
+
 
     // -------------- clean up ---------------------------------------
 
