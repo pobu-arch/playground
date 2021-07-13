af304840798cd0d9eec3864e31faebfc4654932d
bowen <bowen.huang@yale.edu>
2021-07-14 00:23:48 +0800

    updae lat

--- cpp/test_latency/latency.cpp
+++ cpp/test_latency/latency.cpp
@@ -85,7 +85,7 @@ int main()
 
         double load_time = veronica::get_elapsed_time_in_us(1);
 
-        printf("[Info] init took %.2lf s\n", load_time / 1000 / 1000);
+        printf("[Info] init took %.2lf secs\n", load_time / 1000 / 1000);
     }
     else
     {
@@ -117,7 +117,7 @@ int main()
         double load_time = veronica::get_elapsed_time_in_us(0);
         double load_latency =  (load_time * 1000) / amount_of_loads; // nano secs
 
-        printf("total time is %.2lf s, num of loads is %lld, average load latency is %.2lf ns\n", load_time / 1000 / 1000, amount_of_loads, load_latency);
+        printf("pointer chasing total time is %.2lf secs, num of loads is %lld, average load latency is %.2lf ns\n", load_time / 1000 / 1000, amount_of_loads, load_latency);
         fflush(stdout);
 
         current_size *= 1.8;
